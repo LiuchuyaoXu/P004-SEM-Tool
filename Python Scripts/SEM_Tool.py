fc0504aaca8aa73920bf4e05d33fa9387c45bdcf
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2019-10-19 22:38:16 +0100

    Add code for laying out basic GUI.

--- Python Scripts/SEM_Tool.py
+++ Python Scripts/SEM_Tool.py
@@ -11,11 +11,13 @@ import matplotlib.backends.backend_wxagg
 
 class SEM_Tool:
     def __init__(self):
-        self.app = wx.App()
-
-        self.app.frame = wx.Frame(parent=None, title="SEM Realtime Diagnostic Tool")
-        self.app.frame.SetSize(1024, 768)
-        self.app.frame.Centre()
+        app = wx.App()
+        frame = wx.Frame(   parent=None,
+                            title="SEM Realtime Diagnostic Tool",
+                            style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
+        frame.SetMinSize(wx.Size(1280, 720))
+        frame.SetSize(frame.GetMinSize())
+        frame.Centre()
 
         figure = matplotlib.figure.Figure()
         subplots = figure.subplots(nrows=2, ncols=2)
@@ -23,13 +25,44 @@ class SEM_Tool:
         subplots[0,1].set_title("Plot 2")
         subplots[1,0].set_title("Plot 3")
         subplots[1,1].set_title("Plot 4")
-        self.app.frame.canvas = matplotlib.backends.backend_wxagg.FigureCanvasWxAgg(
-                                parent=self.app.frame,
-                                id=-1,
-                                figure=figure)
+        frame.canvas = matplotlib.backends.backend_wxagg.FigureCanvasWxAgg(
+                            parent=frame,
+                            id=-1,
+                            figure=figure)
 
-        self.app.frame.Show()
-        self.app.MainLoop()
+        frame.panel = wx.Panel(parent=frame)
+        plot_choices = ["SEM Image", "SEM Image 2D FFT"]
+        frame.panel.plot1 = wx.RadioBox(parent=frame.panel,
+                                        label="Plot 1",
+                                        choices=plot_choices)
+        frame.panel.plot2 = wx.RadioBox(parent=frame.panel,
+                                        label="Plot 2",
+                                        choices=plot_choices)
+        frame.panel.plot3 = wx.RadioBox(parent=frame.panel,
+                                        label="Plot 3",
+                                        choices=plot_choices)
+        frame.panel.plot4 = wx.RadioBox(parent=frame.panel,
+                                        label="Plot 4",
+                                        choices=plot_choices)
+        frame.panel.zoom = wx.Slider(parent=frame.panel, style=wx.SL_LABELS)
+
+        frame.panel.sizer = wx.BoxSizer(wx.VERTICAL)
+        frame.panel.sizer.Add(frame.panel.plot1)
+        frame.panel.sizer.Add(frame.panel.plot2)
+        frame.panel.sizer.Add(frame.panel.plot3)
+        frame.panel.sizer.Add(frame.panel.plot4)
+        frame.panel.sizer.Add(frame.panel.zoom, flag=wx.EXPAND)
+        frame.panel.SetSizer(frame.panel.sizer)
+
+        frame.sizer = wx.FlexGridSizer(cols=2)
+        frame.sizer.AddGrowableRow(idx=0)
+        frame.sizer.AddGrowableCol(idx=0)
+        frame.sizer.Add(window=frame.canvas, flag=wx.EXPAND)
+        frame.sizer.Add(window=frame.panel, flag=wx.EXPAND)
+        frame.SetSizer(frame.sizer)
+
+        frame.Show()
+        app.MainLoop()
 
 if __name__ == "__main__":
     SEM_Tool()

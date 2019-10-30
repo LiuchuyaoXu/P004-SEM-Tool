b345279c7afe0fbdccf52cb85022230a3e9a76d6
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2019-10-30 23:12:40 +0000

    Switch to Qt from wx for GUI.

---
+++ SEM Realtime Diagnostic Tool/README.md
@@ -0,0 +1,6 @@
+Liuchuyao Xu, 2019
+
+Getting started:
+    Install Qt
+    Install Anaconda Python3.7
+    
---
+++ SEM Realtime Diagnostic Tool/SEMTool.py
@@ -0,0 +1,6 @@
+#   File:   SEMTool.py
+#
+#   Brief:  Implement the SEM realtime diagnostic tool.
+# 
+#   Author: Liuchuyao Xu, 2019
+

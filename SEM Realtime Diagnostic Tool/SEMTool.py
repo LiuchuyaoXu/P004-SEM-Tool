a919d9127e6a9cb9e90a124f028b79f018d07ebe
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2019-11-08 02:58:14 +0000

    Make images refresh freely, fix aspect ratio issue in a dirty way.

--- SEM Realtime Diagnostic Tool/SEMTool.py
+++ SEM Realtime Diagnostic Tool/SEMTool.py
@@ -17,63 +17,86 @@ class SEMTool(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.canvas     = QtWidgets.QWidget()
-        self.layout     = QtWidgets.QGridLayout()
-        self.PILImage   = Image.open("../SEM Images/Armin241.tif")
+        self.panel          = QtWidgets.QDockWidget()
+        self.canvas         = QtWidgets.QWidget()
+        self.canvas.grid    = QtWidgets.QGridLayout()
+
+        self.PILImage   = Image.Image()
         self.image      = QtWidgets.QLabel()
         self.imageXFFT  = QtWidgets.QLabel()
         self.imageYFFT  = QtWidgets.QLabel()
         self.imageXYFFT = QtWidgets.QLabel()
-        self.panel      = QtWidgets.QDockWidget()
-        self.count = 0
 
-        self.updateImage()
-        self.updateImageXFFT()
-        self.updateImageYFFT()
-        self.updateImageXYFFT()
+        self.frameCount = 1
+        self.frameReady = True
+        self.frameTimer = QtCore.QTimer()
+
+        self.initCanvas()
         self.initPanel()
-
-        self.canvas.setLayout(self.layout)
-        self.layout.addWidget(self.image)
-        self.layout.addWidget(self.imageXYFFT)
-        # self.layout.addWidget(self.imageXFFT)
-        # self.layout.addWidget(self.imageYFFT)
 
         self.setWindowTitle("SEM Realtime Diagnostic Tool")
         self.setCentralWidget(self.canvas)
-        self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.panel)
+        # self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.panel)
 
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.updateImage)
-        self.timer.start(1000)
+        self.frameTimer.timeout.connect(self.updateImage)
+        self.frameTimer.start(30)
+
+    def initCanvas(self):
+        self.updateImage()
+        self.canvas.setLayout(self.canvas.grid)
+        self.canvas.grid.addWidget(self.image,      0, 0, QtCore.Qt.AlignCenter)
+        self.canvas.grid.addWidget(self.imageXYFFT, 0, 1, QtCore.Qt.AlignCenter)
+        self.canvas.grid.addWidget(self.imageXFFT,  1, 0, QtCore.Qt.AlignCenter)
+        self.canvas.grid.addWidget(self.imageYFFT,  1, 1, QtCore.Qt.AlignCenter)
 
     def initPanel(self):
         self.panel.setWindowTitle("Control Panel")
-        self.panel.setAllowedAreas(QtCore.Qt.RightDockWidgetArea | 
-                                   QtCore.Qt.LeftDockWidgetArea)
+        self.panel.setAllowedAreas(QtCore.Qt.RightDockWidgetArea)
 
     def updateImage(self):
-        begin = time.time()
-        if self.count == 0:
-            self.PILImage = Image.open("../SEM Images/Armin241.tif")
-            self.count = 1
-        elif self.count == 1:
-            self.PILImage = Image.open("../SEM Images/Armin242.tif")
-            self.count = 0
-        qtImage     = ImageQt.ImageQt(self.PILImage)
-        qtPixmap    = QtGui.QPixmap.fromImage(qtImage)
-        self.image.setPixmap(qtPixmap)
-        self.updateImageXFFT()
-        self.updateImageYFFT()
-        self.updateImageXYFFT()
-        end = time.time()
-        print(end - begin)
+        if self.frameReady:
+            self.frameReady = False
+            begin = time.time()
+
+            self.PILImage   = Image.open("../SEM Images/Armin24%d.tif" % self.frameCount)
+            qtImage         = ImageQt.ImageQt(self.PILImage)
+            qtPixmap        = QtGui.QPixmap.fromImage(qtImage)
+            qtPixmap.setDevicePixelRatio(1.5)
+            self.image.setPixmap(qtPixmap)
+            
+            self.updateImageXFFT()
+            self.updateImageYFFT()
+            self.updateImageXYFFT()
+
+            self.frameCount += 1
+            if self.frameCount == 7:
+                self.frameCount = 1
+
+            end = time.time()
+            print(end - begin)
+            self.frameReady = True
+        else:
+            pass
 
     def updateImageXFFT(self):
-        pass
+        arrayFFT    = np.asarray(self.PILImage)
+        qtImage     = QtGui.QImage(arrayFFT.data, 
+                                   arrayFFT.shape[1],
+                                   arrayFFT.shape[0],
+                                   QtGui.QImage.Format_Indexed8)
+        qtPixmap    = QtGui.QPixmap.fromImage(qtImage)
+        qtPixmap.setDevicePixelRatio(1.5)
+        self.imageXFFT.setPixmap(qtPixmap)
 
     def updateImageYFFT(self):
-        pass
+        arrayFFT    = np.asarray(self.PILImage)
+        qtImage     = QtGui.QImage(arrayFFT.data, 
+                                   arrayFFT.shape[1],
+                                   arrayFFT.shape[0],
+                                   QtGui.QImage.Format_Indexed8)
+        qtPixmap    = QtGui.QPixmap.fromImage(qtImage)
+        qtPixmap.setDevicePixelRatio(1.5)
+        self.imageYFFT.setPixmap(qtPixmap)
 
     def updateImageXYFFT(self):
         array       = np.asarray(self.PILImage)
@@ -89,6 +112,7 @@ class SEMTool(QtWidgets.QMainWindow):
                                    arrayFFT.shape[0],
                                    QtGui.QImage.Format_Indexed8)
         qtPixmap    = QtGui.QPixmap.fromImage(qtImage)
+        qtPixmap.setDevicePixelRatio(1.5)
         self.imageXYFFT.setPixmap(qtPixmap)
 
 if __name__ == "__main__":

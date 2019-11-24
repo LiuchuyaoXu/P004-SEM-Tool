aba78d1080fdf71de3e7fca0bea6f73718800137
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2019-11-24 18:31:56 +0000

    Add code for showing image histogram.

--- SEM Realtime Diagnostic Tool/SEMTool.py
+++ SEM Realtime Diagnostic Tool/SEMTool.py
@@ -14,131 +14,9 @@ from PySide2 import QtCore
 from PySide2 import QtWidgets
 from matplotlib.colors import LogNorm 
 from matplotlib.figure import Figure as MPLFigure
-from matplotlib.backends.backend_qt5agg import FigureCanvas as MPLCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as MPLCanvas
 
-class SEMToolQt(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.panel          = QtWidgets.QDockWidget()
-        self.canvas         = QtWidgets.QWidget()
-        self.canvas.grid    = QtWidgets.QGridLayout()
-
-        self.PILImage   = Image.Image()
-        self.image      = QtWidgets.QLabel()
-        self.imageXFFT  = QtWidgets.QLabel()
-        self.imageYFFT  = QtWidgets.QLabel()
-        self.imageXYFFT = QtWidgets.QLabel()
-
-        self.frameCount = 1
-        self.frameReady = True
-        self.frameTimer = QtCore.QTimer()
-
-        self.initCanvas()
-        self.initPanel()
-
-        self.setWindowTitle("SEM Realtime Diagnostic Tool")
-        self.setCentralWidget(self.canvas)
-        self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.panel)
-
-        self.frameTimer.timeout.connect(self.updateImage)
-        self.frameTimer.start(100)
-
-    def initCanvas(self):
-        self.updateImage()
-        self.canvas.setLayout(self.canvas.grid)
-        self.canvas.grid.addWidget(self.image,      0, 0, QtCore.Qt.AlignCenter)
-        self.canvas.grid.addWidget(self.imageXYFFT, 0, 1, QtCore.Qt.AlignCenter)
-        self.canvas.grid.addWidget(self.imageXFFT,  1, 0, QtCore.Qt.AlignCenter)
-        self.canvas.grid.addWidget(self.imageYFFT,  1, 1, QtCore.Qt.AlignCenter)
-
-    def initPanel(self):
-        slider = QtWidgets.QSlider(QtCore.Qt.Horizontal, self.panel)
-
-        grid = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.Direction.TopToBottom)
-        grid.addWidget(slider)
-
-        self.panel.setLayout(grid)
-        self.panel.setWindowTitle("Control Panel")
-        self.panel.setAllowedAreas(QtCore.Qt.RightDockWidgetArea)
-
-    def updateImage(self):
-        if self.frameReady:
-            self.frameReady = False
-            begin = time.time()
-
-            self.PILImage   = Image.open("../SEM Images/Armin24%d.tif" % self.frameCount)
-            qtImage         = ImageQt.ImageQt(self.PILImage)
-            qtPixmap        = QtGui.QPixmap.fromImage(qtImage)
-            qtPixmap.setDevicePixelRatio(1.5)
-            self.image.setPixmap(qtPixmap)
-            
-            self.updateImageXFFT()
-            self.updateImageYFFT()
-            self.updateImageXYFFT()
-
-            self.frameCount += 1
-            if self.frameCount == 7:
-                self.frameCount = 1
-
-            end = time.time()
-            print(end - begin)
-            self.frameReady = True
-        else:
-            pass
-
-    def updateImageXFFT(self):
-        array       = np.asarray(self.PILImage)
-        arrayFFT    = np.fft.fft2(array)
-        arrayFFT    = np.fft.fftshift(arrayFFT)
-        arrayFFT    = np.abs(arrayFFT)
-        arrayFFT    = np.log(arrayFFT)
-        arrayFFT    = arrayFFT / np.max(arrayFFT)
-        arrayFFT    = 255 * arrayFFT
-        arrayFFT    = np.require(arrayFFT, np.uint8, 'C')
-        qtImage     = QtGui.QImage(arrayFFT.data, 
-                                   arrayFFT.shape[1],
-                                   arrayFFT.shape[0],
-                                   QtGui.QImage.Format_Indexed8)
-        qtPixmap    = QtGui.QPixmap.fromImage(qtImage)
-        qtPixmap.setDevicePixelRatio(1.5)
-        self.imageXFFT.setPixmap(qtPixmap)
-
-    def updateImageYFFT(self):
-        array       = np.asarray(self.PILImage)
-        arrayFFT    = np.fft.fft2(array)
-        arrayFFT    = np.fft.fftshift(arrayFFT)
-        arrayFFT    = np.abs(arrayFFT)
-        arrayFFT    = np.log(arrayFFT)
-        arrayFFT    = arrayFFT / np.max(arrayFFT)
-        arrayFFT    = 255 * arrayFFT
-        arrayFFT    = np.require(arrayFFT, np.uint8, 'C')
-        qtImage     = QtGui.QImage(arrayFFT.data, 
-                                   arrayFFT.shape[1],
-                                   arrayFFT.shape[0],
-                                   QtGui.QImage.Format_Indexed8)
-        qtPixmap    = QtGui.QPixmap.fromImage(qtImage)
-        qtPixmap.setDevicePixelRatio(1.5)
-        self.imageYFFT.setPixmap(qtPixmap)
-
-    def updateImageXYFFT(self):
-        array       = np.asarray(self.PILImage)
-        arrayFFT    = np.fft.fft2(array)
-        arrayFFT    = np.fft.fftshift(arrayFFT)
-        arrayFFT    = np.abs(arrayFFT)
-        arrayFFT    = np.log(arrayFFT)
-        arrayFFT    = arrayFFT / np.max(arrayFFT)
-        arrayFFT    = 255 * arrayFFT
-        arrayFFT    = np.require(arrayFFT, np.uint8, 'C')
-        qtImage     = QtGui.QImage(arrayFFT.data, 
-                                   arrayFFT.shape[1],
-                                   arrayFFT.shape[0],
-                                   QtGui.QImage.Format_Indexed8)
-        qtPixmap    = QtGui.QPixmap.fromImage(qtImage)
-        qtPixmap.setDevicePixelRatio(1.5)
-        self.imageXYFFT.setPixmap(qtPixmap)
-
-class SEMToolMPL(QtWidgets.QMainWindow):
+class SEMTool(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
@@ -148,6 +26,7 @@ class SEMToolMPL(QtWidgets.QMainWindow):
         self.initCanvas()
 
         self.setCentralWidget(self.canvas)
+        self.setWindowTitle("SEM Realtime Diagnostic Tool")
 
     def initCanvas(self):
         plots = self.canvas.figure.subplots(nrows=2, ncols=2)
@@ -163,10 +42,10 @@ class SEMToolMPL(QtWidgets.QMainWindow):
         self.imageXFFT.axis('off')
         self.imageYFFT.axis('off')
 
-        self.image.set_title("Original Image")
-        self.imageXYFFT.set_title("2D FFT")
-        self.imageXFFT.set_title("X-Axis FFT")
-        self.imageYFFT.set_title("Y-Axis FFT")
+        # self.image.set_title("Original Image")
+        # self.imageXYFFT.set_title("2D FFT")
+        # self.imageXFFT.set_title("X-Axis FFT")
+        # self.imageYFFT.set_title("Y-Axis FFT")
 
         self.frameCount = 1
         self.frameReady = True
@@ -177,10 +56,13 @@ class SEMToolMPL(QtWidgets.QMainWindow):
         self.arrayXYFFT = np.fft.fft2(self.array)
         self.arrayXYFFT = np.fft.fftshift(self.arrayXYFFT)
         self.arrayXYFFT = np.abs(self.arrayXYFFT)
+
+        self.imageXFFT.hist(np.matrix.flatten(self.array), bins=10)
+
         self.img        = self.image.imshow(self.SEMImage, cmap = 'gray')
-        self.imgXYFFT   = self.imageXYFFT.imshow(self.arrayXYFFT, norm = LogNorm())
-        self.imgXFFT    = self.imageXFFT.imshow(self.arrayXYFFT, norm = LogNorm())
+        # self.imgXFFT    = self.imageXFFT.imshow(self.arrayXYFFT, norm = LogNorm())
         self.imgYFFT    = self.imageYFFT.imshow(self.arrayXYFFT, norm = LogNorm())
+        self.imgXYFFT   = self.imageXYFFT.imshow(self.arrayXYFFT, norm = LogNorm())
 
         self.updateImage()
 
@@ -202,10 +84,14 @@ class SEMToolMPL(QtWidgets.QMainWindow):
             # self.imageXYFFT.set_xlim(-1000, 1000)
             # self.imageXYFFT.set_ylim(-1000, 1000)
 
+            self.imageXFFT.clear()
+            self.imageXFFT.hist(np.matrix.flatten(self.array), bins=10)
+
             self.img.set_data(self.SEMImage)
-            self.imgXYFFT.set_data(self.arrayXYFFT)
-            self.imgXFFT.set_data(self.arrayXYFFT)
+            # self.imgXFFT.set_data(self.arrayXYFFT)
             self.imgYFFT.set_data(self.arrayXYFFT)
+            self.imgXYFFT.set_data(self.arrayXYFFT)
+
             self.canvas.figure.canvas.draw()
 
             self.frameCount += 1
@@ -220,6 +106,6 @@ class SEMToolMPL(QtWidgets.QMainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    gui = SEMToolMPL()
+    gui = SEMTool()
     gui.show()
     sys.exit(app.exec_())

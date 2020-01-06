de2246636ba7f2e96d5e0e1d2e9bdcd204a37965
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2020-01-06 23:04:54 +0000

    Add test file.

---
+++ SEM Realtime Diagnostic Tool/semToolTest.py
@@ -0,0 +1,109 @@
+#   File:   semToolTest.py
+#
+#   Brief:  Implement the SEM real-time diagnostic tool.
+# 
+#   Author: Liuchuyao Xu, 2019
+
+import sys
+import time
+import numpy as np
+from PIL import Image
+from PySide2 import QtGui
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+from matplotlib.figure import Figure as MplFigure
+from matplotlib.colors import LogNorm as MplLogNorm
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as MplCanvas
+
+class semTool(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.semImage   = Image.Image()
+        self.canvas     = MplCanvas(MplFigure(constrained_layout=True))
+
+        self.initCanvas()
+
+        self.setCentralWidget(self.canvas)
+        self.setWindowTitle("SEM Real-time Diagnostic Tool")
+
+    def initCanvas(self):
+        plots = self.canvas.figure.subplots(nrows=2, ncols=2)
+        # self.canvas.figure.subplots_adjust(hspace=0, wspace=0)
+
+        self.image      = plots[0, 0]
+        self.imageXYFFT = plots[0, 1]
+        self.imageXFFT  = plots[1, 0]
+        self.imageYFFT  = plots[1, 1]
+
+        self.image.axis('off')
+        self.imageXYFFT.axis('off')
+        self.imageXFFT.axis('off')
+        self.imageYFFT.axis('off')
+
+        # self.image.set_title("Original Image")
+        # self.imageXYFFT.set_title("2D FFT")
+        # self.imageXFFT.set_title("X-Axis FFT")
+        # self.imageYFFT.set_title("Y-Axis FFT")
+
+        self.frameCount = 1
+        self.frameReady = True
+        self.frameTimer = QtCore.QTimer()
+
+        self.semImage   = Image.open("../SEM Images/Armin24%d.tif" % self.frameCount)
+        self.array      = np.asarray(self.semImage)
+        self.arrayXYFFT = np.fft.fft2(self.array)
+        self.arrayXYFFT = np.fft.fftshift(self.arrayXYFFT)
+        self.arrayXYFFT = np.abs(self.arrayXYFFT)
+
+        self.imageXFFT.hist(np.matrix.flatten(self.array), bins=10)
+
+        self.img        = self.image.imshow(self.array, cmap = 'gray')
+        # self.imgXFFT    = self.imageXFFT.imshow(self.arrayXYFFT, norm = MplLogNorm())
+        self.imgYFFT    = self.imageYFFT.imshow(self.arrayXYFFT, norm = MplLogNorm())
+        self.imgXYFFT   = self.imageXYFFT.imshow(self.arrayXYFFT, norm = MplLogNorm())
+
+        self.updateImage()
+
+        self.frameTimer.timeout.connect(self.updateImage)
+        self.frameTimer.start(100)
+
+    def updateImage(self):
+        if self.frameReady:
+            self.frameReady = False
+            begin = time.time()
+
+            self.semImage   = Image.open("../SEM Images/Armin24%d.tif" % self.frameCount)
+            self.array      = np.asarray(self.semImage)
+            self.arrayXYFFT = np.fft.fft2(self.array)
+            self.arrayXYFFT = np.fft.fftshift(self.arrayXYFFT)
+            self.arrayXYFFT = np.abs(self.arrayXYFFT)
+
+            # self.imageXYFFT.set_xlim(-1000, 1000)
+            # self.imageXYFFT.set_ylim(-1000, 1000)
+
+            self.imageXFFT.clear()
+            self.imageXFFT.hist(np.matrix.flatten(self.array), bins=10)
+
+            self.img.set_data(self.array)
+            # self.imgXFFT.set_data(self.arrayXYFFT)
+            self.imgYFFT.set_data(self.arrayXYFFT)
+            self.imgXYFFT.set_data(self.arrayXYFFT)
+
+            self.canvas.figure.canvas.draw()
+
+            self.frameCount += 1
+            if self.frameCount == 7:
+                self.frameCount = 1
+
+            end = time.time()
+            print(end - begin)
+            self.frameReady = True
+        else:
+            pass
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    gui = semTool()
+    gui.show()
+    sys.exit(app.exec_())

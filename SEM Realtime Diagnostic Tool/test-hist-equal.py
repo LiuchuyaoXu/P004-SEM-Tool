0b52ecdc7dc114dee2769fbcd3dfc569ac3e1b98
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2020-01-25 20:02:57 +0000

    Add gray-level histogram equalization algorithm.

--- SEM Realtime Diagnostic Tool/test-hist-equal.py
+++ SEM Realtime Diagnostic Tool/test-hist-equal.py
@@ -1,45 +1,59 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-def get_histogram(image, bins):
-    # array with size of bins, set to zeros
-    histogram = np.zeros(bins)
-    
-    # loop through pixels and sum up counts of pixels
-    for pixel in image:
-        histogram[pixel] += 1
-    
-    # return our final result
-    return histogram
+class semImage(np.ndarray):
+    def __init__(self):
+        super().__init__()
 
-def cumsum(a):
-    a = iter(a)
-    b = [next(a)]
-    for i in a:
-        b.append(b[-1] + i)
-    return np.array(b)
+    def getFlat(self):
+        return np.matrix.flatten(self)
+
+    def getHist(self):
+        hist = np.zeros(256)
+        for pixel in self.getFlat():
+            hist[pixel] += 1
+        return hist
+
+    def getHistEqualized(self):
+        start = time.time()
+        hist        = self.getHist()
+        histTrans   = np.zeros(256)
+        numPixels   = np.sum(hist)
+
+        summ = 0
+        for i in range(0, 256):
+            summ += hist[i]
+            histTrans[i] = summ
+        histTrans /= numPixels
+        histTrans *= 255 / max(histTrans)
+        histTrans = histTrans.astype(int)
+
+        shape = self.shape
+        newImage = np.zeros(shape).astype(int)
+        for i in range(0, shape[0]):
+            for j in range(0, shape[1]):
+                newImage[i, j] = histTrans[self[i, j]]
+
+        newHist = np.zeros(256)
+        for pixel in np.matrix.flatten(newImage):
+            newHist[pixel] += 1
+        end = time.time()
+        print(end - start)
+        return newImage, newHist
 
 if __name__ == "__main__":
-    img     = Image.open("../SEM Images/Armin241.tif")
-    img     = np.asarray(img)
-    flat    = img.flatten()
+    image = Image.open("SEM Images/Armin241.tif")
+    image = np.asarray(image)
+    image = image.view(semImage)
 
-    hist    = get_histogram(flat, 256)
-    cs      = cumsum(hist)
-    nj      = (cs - cs.min()) * 255
-    N       = cs.max() - cs.min()
-    cs      = nj / N
-    cs      = cs.astype('uint8')
-    img_new = cs[flat]
-    img_new = np.reshape(img_new, img.shape)
+    imageHist               = image.getHist()
+    newImage, newImageHist  = image.getHistEqualized()
 
-    fig     = plt.figure()
-
-    fig.add_subplot(1, 2, 1)
-    plt.imshow(img, cmap='gray')
-
-    fig.add_subplot(1, 2, 2)
-    plt.imshow(img_new, cmap='gray')
-
+    fig, a = plt.subplots(2, 2)
+    a[0][0].imshow(image, cmap="gray")
+    a[0][1].bar(np.arange(0, imageHist.size), imageHist)
+    a[1][0].imshow(newImage, cmap="gray")
+    a[1][1].bar(np.arange(0, newImageHist.size), newImageHist)
     plt.show()

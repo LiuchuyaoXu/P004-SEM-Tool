cf1d6b86124203ef916244f0f50df3db77cee626
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2020-03-06 18:10:23 +0000

    Add Masker module.

---
+++ Application/Masker.py
@@ -0,0 +1,105 @@
+# Author:   Liuchuyao Xu, 2020
+#
+# Brief:    Implement the Masker class, which Provides 8 masks dividing a 2d shape into 8 non-overlapping regions, as specified in K.H. ONG, J.C.H. PHANG, J.T.L. THONG, 1997, A Robust Focusing and Astigmatism Correction Method for the Scanning Electron Microscope.
+
+import numpy as np
+
+class Masker:
+    @property
+    def r1(self):
+        return self._r1
+
+    @property
+    def r2(self):
+        return self._r2
+
+    @property
+    def r3(self):
+        return self._r3
+
+    @property
+    def r4(self):
+        return self._r4
+
+    @property
+    def s1(self):
+        return self._s1
+
+    @property
+    def s2(self):
+        return self._s2
+
+    @property
+    def s3(self):
+        return self._s3
+
+    @property
+    def s4(self):
+        return self._s4
+
+    def __init__(self, shape):
+        self._r1 = np.ones(shape)
+        self._r2 = np.ones(shape)
+        self._r3 = np.ones(shape)
+        self._r4 = np.ones(shape)
+        self._s1 = np.ones(shape)
+        self._s2 = np.ones(shape)
+        self._s3 = np.ones(shape)
+        self._s4 = np.ones(shape)
+
+        xLen = shape[0]
+        yLen = shape[1]
+        xOri = np.floor(xLen / 2)
+        yOri = np.floor(yLen / 2)
+        for i in range(0, xLen):
+            for j in range(0, yLen):
+                x = i - xOri
+                y = j - yOri
+                if x == 0:
+                    if y == 0:
+                        pass
+                    elif y > 0:
+                        self._r1[i][j] = 0
+                    else:
+                        self._r2[i][j] = 0
+                elif x > 0:
+                    if y == 0:
+                        self._r4[i][j] = 0
+                    else:
+                        angle = np.arctan(y/x)
+                        if angle < (- 3 * np.pi / 8):
+                            self._r2[i][j] = 0
+                        elif angle < (- np.pi / 8):
+                            self._s2[i][j] = 0
+                        elif angle < (np.pi / 8):
+                            self._r4[i][j] = 0
+                        elif angle < (3 * np.pi / 8):
+                            self._s4[i][j] = 0
+                        else:
+                            self._r1[i][j] = 0
+                else:
+                    if y == 0:
+                        self._r3[i][j] = 0
+                    else:
+                        angle = np.arctan(y/x)
+                        if angle < (- 3 * np.pi / 8):
+                            self._r1[i][j] = 0
+                        elif angle < (- np.pi / 8):
+                            self._s1[i][j] = 0
+                        elif angle < (np.pi / 8):
+                            self._r3[i][j] = 0
+                        elif angle < (3 * np.pi / 8):
+                            self._s3[i][j] = 0
+                        else:
+                            self._r2[i][j] = 0
+
+if __name__ == "__main__":
+    masker = Masker([5, 7])
+    print(masker.r1)
+    print(masker.r2)
+    print(masker.r3)
+    print(masker.r4)
+    print(masker.s1)
+    print(masker.s2)
+    print(masker.s3)
+    print(masker.s4)

dc180f94bb5712995de796a9aaef71546a9cfed3
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2020-06-28 23:06:36 +0100

    Update.

--- Application/SemController.py
+++ Application/SemController.py
@@ -2,7 +2,12 @@
 #
 #   Author: Liuchuyao Xu, 2020
 #
-#   Brief:  Provides an interface for controlling the SEM.
+#   Brief:  Implement the SemController class.
+#           The class gives access to the API of the SEM
+#           The class provides functions that the API does not support directly.
+#
+#   Abbreviations:
+#           ole     Microsoft Object Linking and Embedding document
 
 import sys
 import tempfile
@@ -13,52 +18,17 @@ class SemController:
 
     def __init__(self):
         ole = 'CZ.EmApiCtrl.1'
-        # sys.argv = ['makepy', ole]
-        # client.makepy.main()
+        self.sem = client.Dispatch(ole)
+        self.sem.InitialiseRemoting()
 
-        self.mic = client.Dispatch(ole)
-        self.mic.InitialiseRemoting()
+    def __call__(self):
+        return self.sem
 
-    def getValue(self, name):
-        return self.mic.Get(name, 0.0)
-
-    def setValue(self, name, value):
-        return self.mic.Set(name, str(value))
-
-    def grabImage(self):
+    def grabImage(self, xOffset=0, yOffset=0, width=1024, height=768, reduction=0):
         filename = tempfile.TemporaryFile(suffix='.bmp').name
-        self.mic.Grab(0, 0, 1024, 768, 0, filename)
+        self.sem.Grab(xOffset, yOffset, width, height, reduction, filename)
         image = Image.open(filename)
         return image
 
-    # @property
-    # def AP_WD(self):
-    #     return self.mic.Get('AP_WD', 0.0)
-
-    # @property
-    # def AP_STIG_X(self):
-    #     return self.mic.Get('AP_STIG_X', 0.0)
-
-    # @property
-    # def AP_STIG_Y(self):
-    #     return self.mic.Get('AP_STIG_Y', 0.0)
-
-    # @AP_WD.setter
-    # def AP_WD(self, value):
-    #     return self.mic.Set('AP_WD', str(value))
-
-    # @AP_STIG_X.setter
-    # def AP_STIG_X(self, value):
-    #     return self.mic.Set('AP_STIG_X', str(value))
-
-    # @AP_STIG_Y.setter
-    # def AP_STIG_Y(self, value):
-    #     return self.mic.Set('AP_STIG_Y', str(value))
-
 if __name__ == '__main__':
     sem = SemController()
-
-    res = sem.setValue("AP_RED_RASTER_POSN_X", 256)
-    res = sem.setValue("AP_RED_RASTER_POSN_Y", 128)
-    res = sem.setValue("AP_RED_RASTER_W", 512)
-    res = sem.setValue("AP_RED_RASTER_H", 512)
--- Application/SemCorrector.py
+++ Application/SemCorrector.py
@@ -2,19 +2,18 @@
 # 
 #   Author: Liuchuyao Xu, 2020
 #
-#   Brief: Implement an automatic focusing and astigmatism correction algorithm.
+#   Brief:  Implement an automatic focusing and astigmatism correction algorithm.
 
 import time
 import numpy.ma as ma
 import matplotlib.pyplot as plt
 
-import SEM_API
 from Segmenter import Segmenter
+from SemController import SemController
 from SemImage import SemImage
 
 class SemCorrector():
 
-    sem = None
     segmenter = None
 
     wdIters = []
@@ -23,14 +22,28 @@ class SemCorrector():
 
     wdStep = 0.02 # In mm
     wdOffset = 0.10 # In mm.
-    dP_threshold = 0.002
-    sxyStep = 0.50
-    dP_rs_threshold = 0.02
+    dP_threshold = 0.05
+    sxyStep = 5
+    dP_rs_threshold = 0.002
 
-    def __init__(self, sem):
-        self.sem = sem
-        self.sem.UpdateImage_Start()
-        self.segmenter = Segmenter([1024, 768])
+    xOffset = 512
+    yOffset = 256
+    width = 256
+    height = 256
+
+    focusCorrected = False
+    stigmaCorrected = False
+
+    def __init__(self):
+        self.sem = SemController()
+        self.sem().UpdateImage_Start()
+        self.segmenter = Segmenter([self.width, self.height])
+
+        self.sem().Execute("CMD_MODE_REDUCED")
+        self.sem().Set("AP_RED_RASTER_POSN_X", str(self.xOffset))
+        self.sem().Set("AP_RED_RASTER_POSN_Y", str(self.yOffset))
+        self.sem().Set("AP_RED_RASTER_W", str(self.width))
+        self.sem().Set("AP_RED_RASTER_H", str(self.height))
 
     def start(self):
         iters = range(20)
@@ -53,10 +66,10 @@ class SemCorrector():
         print("--------------------")
         print(" ")
         print("Start iteration.")
-        wd = self.sem.GetValue("AP_WD") * 1000   # In mm.
-        sx = self.sem.GetValue("AP_STIG_X")
-        sy = self.sem.GetValue("AP_STIG_Y")
-        ft = self.sem.GetValue("AP_FRAME_TIME") / 1000 # In seconds.
+        wd = self.sem().GetValue("AP_WD") * 1000   # In mm.
+        sx = self.sem().GetValue("AP_STIG_X")
+        sy = self.sem().GetValue("AP_STIG_Y")
+        ft = self.sem().GetValue("AP_FRAME_TIME") / 1000 # In seconds.
         print(" ")
         print("Initial settings: ")
         print("Working distance: ", wd, "mm")
@@ -67,12 +80,12 @@ class SemCorrector():
         self.sxIters.append(sx)
         self.syIters.append(sy)
 
-        self.sem.SetValue("AP_WD", wd-self.wdOffset)
-        time.sleep(ft)
-        imageUf = SemImage.create(self.sem.img_array)
+        self.sem().SetValue("AP_WD", wd-self.wdOffset)
+        time.sleep(3*ft)
+        imageUf = SemImage.create(self.sem.grabImage(self.xOffset, self.yOffset, self.width, self.height))
         imageUf.applyHanning()
         imageUf.updateFft()
-        P_uf, P_r12_uf, P_r34_uf, P_s12_uf, P_s34_uf = self.segmentFft(self.segmenter, imageUf.fft)
+        P_uf, P_r12_uf, P_r34_uf, P_s12_uf, P_s34_uf = self.segmentFft(imageUf.fft)
         print(" ")
         print("Underfocused image FFT:")
         print("P_uf:     ", P_uf)
@@ -81,12 +94,12 @@ class SemCorrector():
         print("P_s12_uf: ", P_s12_uf)
         print("P_s34_uf: ", P_s34_uf)
 
-        self.sem.SetValue("AP_WD", wd+self.wdOffset)
-        time.sleep(ft)       
-        imageOf = SemImage.create(self.sem.img_array)
+        self.sem().SetValue("AP_WD", wd+self.wdOffset)
+        time.sleep(3*ft)       
+        imageOf = SemImage.create(self.sem.grabImage(self.xOffset, self.yOffset, self.width, self.height))
         imageOf.applyHanning()
         imageOf.updateFft()
-        P_of, P_r12_of, P_r34_of, P_s12_of, P_s34_of = self.segmentFft(self.segmenter, imageOf.fft)
+        P_of, P_r12_of, P_r34_of, P_s12_of, P_s34_of = self.segmentFft(imageOf.fft)
         print(" ")
         print("Overfocused image FFT:")
         print("P_of:     ", P_of)
@@ -95,7 +108,7 @@ class SemCorrector():
         print("P_s12_of: ", P_s12_of)
         print("P_s34_of: ", P_s34_of)
 
-        self.sem.SetValue("AP_WD", wd)
+        self.sem().SetValue("AP_WD", wd)
 
         dP = (P_of - P_uf) / (P_of + P_uf)
         dP_r12 = (P_r12_of - P_r12_uf) / (P_r12_of + P_r12_uf)
@@ -110,24 +123,31 @@ class SemCorrector():
         print("dP_s12: ", dP_s12)
         print("dP_s34: ", dP_s34)
 
-        if abs(dP) > self.dP_threshold:
-            self.adjustFocus(dP, wd)        
-        elif abs(dP_r12) > self.dP_rs_threshold and abs(dP_r34) > self.dP_rs_threshold:
-            self.adjustStigmaX(dP_r12, dP_r34, sx)
-        elif abs(dP_s12) > self.dP_rs_threshold and abs(dP_s34) > self.dP_rs_threshold:
-            self.adjustStigmaX(dP_s12, dP_s34, sy)
+        if not self.focusCorrected:
+            if abs(dP) > self.dP_threshold:
+                self.adjustFocus(dP, wd)        
+            else:
+                self.focusCorrected = True
 
-    def segmentFft(self, segmenter, fft):
-        fft = fft > 50000
+        if self.focusCorrected:
+            if abs(dP_r12) > self.dP_rs_threshold or abs(dP_r34) > self.dP_rs_threshold:
+                self.adjustStigmaX(dP_r12, dP_r34, sx)
+            # elif abs(dP_s12) > self.dP_rs_threshold or abs(dP_s34) > self.dP_rs_threshold:
+            #     self.adjustStigmaY(dP_s12, dP_s34, sy)
+
+        time.sleep(3 * ft)
+
+    def segmentFft(self, fft):
+        fft = fft > 3000
         P = fft.sum()
-        P_r1 = ma.array(fft, segmenter.r1).sum()
-        P_r2 = ma.array(fft, segmenter.r2).sum()
-        P_r3 = ma.array(fft, segmenter.r3).sum()
-        P_r4 = ma.array(fft, segmenter.r4).sum()
-        P_s1 = ma.array(fft, segmenter.s1).sum()
-        P_s2 = ma.array(fft, segmenter.s2).sum()
-        P_s3 = ma.array(fft, segmenter.s3).sum()
-        P_s4 = ma.array(fft, segmenter.s4).sum()
+        P_r1 = ma.array(fft, self.segmenter.r1).sum()
+        P_r2 = ma.array(fft, self.segmenter.r2).sum()
+        P_r3 = ma.array(fft, self.segmenter.r3).sum()
+        P_r4 = ma.array(fft, self.segmenter.r4).sum()
+        P_s1 = ma.array(fft, self.segmenter.s1).sum()
+        P_s2 = ma.array(fft, self.segmenter.s2).sum()
+        P_s3 = ma.array(fft, self.segmenter.s3).sum()
+        P_s4 = ma.array(fft, self.segmenter.s4).sum()
         P_r12 = P_r1 + P_r2
         P_r34 = P_r3 + P_r4
         P_s12 = P_s1 + P_s2
@@ -137,33 +157,36 @@ class SemCorrector():
     def adjustFocus(self, dP, wd):
         print(" ")
         if dP > 0:
-            self.sem.SetValue("AP_WD", wd+self.wdStep)
+            self.sem().Set("AP_WD", str(wd+self.wdStep))
             print("Increased working distance.")
         else:
-            self.sem.SetValue("AP_WD", wd-self.wdStep)
+            self.sem().Set("AP_WD", str(wd-self.wdStep))
             print("Decreased working distance.")
 
     def adjustStigmaX(self, dP_r12, dP_r34, sx):
-        if dP_r12 > 0 and dP_r34 < 0:
-            self.sem.SetValue("AP_STIG_X", sx-self.sxyStep)
+        # if dP_r12 > 0 and dP_r34 < 0:
+        if dP_r12 - dP_r34 > self.dP_rs_threshold:
+            self.sem().Set("AP_STIG_X", str(sx-self.sxyStep))
             print(" ")
             print("Decreased stigmator X.")
-        elif dP_r12 < 0 and dP_r34 > 0:
-            self.sem.SetValue("AP_STIG_X", sx+self.sxyStep)
+        # elif dP_r12 < 0 and dP_r34 > 0:
+        elif dP_r34 - dP_r12 > self.dP_rs_threshold:
+            self.sem().Set("AP_STIG_X", str(sx+self.sxyStep))
             print(" ")
             print("Increased stigmator X.")
 
     def adjustStigmaY(self, dP_s12, dP_s34, sy):
-        if dP_s12 > 0 and dP_s34 < 0:
-            self.sem.SetValue("AP_STIG_Y", sy-self.sxyStep)
+        # if dP_s12 > 0 and dP_s34 < 0:
+        if dP_s12 - dP_s34 > self.dP_rs_threshold:
+            self.sem().Set("AP_STIG_Y", str(sy-self.sxyStep))
             print(" ")
             print("Decreased stigmator Y.")
-        elif dP_s12 < 0 and dP_s34 > 0:
-            self.sem.SetValue("AP_STIG_Y", sy+self.sxyStep)
+        # elif dP_s12 < 0 and dP_s34 > 0:
+        elif dP_s34 - dP_s12 > self.dP_rs_threshold:
+            self.sem().Set("AP_STIG_Y", str(sy+self.sxyStep))
             print(" ")
             print("Increased stigmator Y.")
 
 if __name__ == "__main__":
-    with SEM_API.SEM_API("remote") as sem:
-        semCorrector = SemCorrector(sem)
-        semCorrector.start()
+    semCorrector = SemCorrector()
+    semCorrector.start()

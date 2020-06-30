3a73123461198791da33aadba5c66baa5cba9ce5
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2020-07-01 00:38:05 +0100

    Update new SemTool.

--- Application/SemToolNew.py
+++ Application/SemToolNew.py
@@ -4,12 +4,18 @@
 # 
 #   Brief:  Implement classes related to the GUI of the SEM diagnostic tool.
 
+import os
 import sys
+from PIL import Image
 from PySide2 import QtGui
 from PySide2 import QtCore
 from PySide2 import QtWidgets
 
+from SemImage import SemImage
+
 class SemTool(QtWidgets.QWidget):
+    localImageFolder = None
+
     frameUpdated = QtCore.Signal()
 
     def __init__(self):
@@ -21,23 +27,22 @@ class SemTool(QtWidgets.QWidget):
         self.initImageSourceBox()
         self.initProcessingAlgorithmsBox()
         self.initPlotsBox()
+        self.initCommandBox()
 
         layout = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom, self)
         layout.insertWidget(0, self.imageSourceBox)
         layout.insertWidget(1, self.processingAlgorithmsBox)
         layout.insertWidget(2, self.plotsBox)
-
-        self.frameUpdated.connect(self.updateFrame, QtCore.Qt.QueuedConnection)
-        self.updateFrame()
+        layout.insertWidget(3, self.commandBox)
 
     def initImageSourceBox(self):
         self.imageSourceBox = QtWidgets.QGroupBox('Image Source')
 
-        folderButton = QtWidgets.QRadioButton('Local Folder')
-        folderButton.setChecked(True)
-        folderButton.toggled.connect(self.onSelectFolderAsImageSource)
-        semButton = QtWidgets.QRadioButton('SEM')
-        semButton.toggled.connect(self.onSelectSemAsImageSource)
+        self.folderButton = QtWidgets.QRadioButton('Local Folder')
+        self.folderButton.setChecked(True)
+        self.folderButton.toggled.connect(self.onSelectFolderAsImageSource)
+        self.semButton = QtWidgets.QRadioButton('SEM')
+        self.semButton.toggled.connect(self.onSelectSemAsImageSource)
 
         self.browseButton = QtWidgets.QPushButton('Browse')
         self.browseButton.clicked.connect(self.browseForImageFolder)
@@ -45,8 +50,8 @@ class SemTool(QtWidgets.QWidget):
         self.folderText.setWordWrap(True)
 
         layout = QtWidgets.QGridLayout()
-        layout.addWidget(folderButton, 0, 0)
-        layout.addWidget(semButton, 0, 1)
+        layout.addWidget(self.folderButton, 0, 0)
+        layout.addWidget(self.semButton, 0, 1)
         layout.addWidget(self.browseButton, 1, 0)
         layout.addWidget(self.folderText, 1, 1)
         self.imageSourceBox.setLayout(layout)
@@ -77,11 +82,21 @@ class SemTool(QtWidgets.QWidget):
         layout.addWidget(fftDistributionBox, 1, 1)
         self.plotsBox.setLayout(layout)
 
+    def initCommandBox(self):
+        self.commandBox = QtWidgets.QGroupBox('')
+
+        self.toggleFrameUpdateButton = QtWidgets.QPushButton('Start Updating Frames')
+        self.toggleFrameUpdateButton.clicked.connect(self.startFrameUpdate)
+        
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.toggleFrameUpdateButton, 0, 0, alignment=QtCore.Qt.AlignCenter)
+        self.commandBox.setLayout(layout)
+
     def browseForImageFolder(self):
         folder = QtWidgets.QFileDialog.getExistingDirectory()
         self.folderText.setText(folder)
         self.localImageFolder = QtCore.QDir(folder)
-        self.localImages = self.localImageFolder.entryList()
+        self.localImages = self.localImageFolder.entryList(['*.tif'])
         self.localImagesIndex = 0
 
     def onSelectFolderAsImageSource(self, checked):
@@ -94,7 +109,31 @@ class SemTool(QtWidgets.QWidget):
             self.browseButton.setEnabled(False)
             self.folderText.setEnabled(False)
 
+    def startFrameUpdate(self):
+        if not self.localImageFolder:
+            print('Please specify the local folder.')
+            return
+
+        self.toggleFrameUpdateButton.setText('Stop Updating Frames')
+        self.toggleFrameUpdateButton.clicked.disconnect(self.startFrameUpdate)
+        self.toggleFrameUpdateButton.clicked.connect(self.stopFrameUpdate)
+        self.frameUpdated.connect(self.updateFrame, QtCore.Qt.QueuedConnection)
+        self.updateFrame()
+
+    def stopFrameUpdate(self):
+        self.toggleFrameUpdateButton.setText('Start Updating Frames')
+        self.toggleFrameUpdateButton.clicked.disconnect(self.stopFrameUpdate)
+        self.toggleFrameUpdateButton.clicked.connect(self.startFrameUpdate)
+        self.frameUpdated.disconnect(self.updateFrame)
+
     def updateFrame(self):
+        if self.folderButton.isChecked():
+            path = os.path.join(self.localImageFolder.path(), self.localImages[self.localImagesIndex])
+            image = SemImage.create(Image.open(path))
+            self.localImagesIndex += 1
+            if self.localImagesIndex >= len(self.localImages):
+                self.localImagesIndex = 0
+
         self.frameUpdated.emit()
 
 if __name__ == '__main__':

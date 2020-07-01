20968778de950587fa536485a1f2693e3671429f
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2020-07-01 16:18:28 +0100

    Complete feature of the plot checkboxes.

--- Application/SemToolNew.py
+++ Application/SemToolNew.py
@@ -14,6 +14,60 @@ from PySide2 import QtWidgets
 from SemImage import SemImage
 from SemController import SemController
 
+class ImagePlot(QtWidgets.QLabel):
+    closed = QtCore.Signal()
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Image')
+
+    def update(self, semImage):
+        width = semImage.image.shape[1]
+        height = semImage.image.shape[0]
+        image = QtGui.QImage(semImage.image, width, height, QtGui.QImage.Format_Grayscale8)
+        self.setPixmap(QtGui.QPixmap(image))
+
+    def closeEvent(self, event):
+        event.accept()
+        self.closed.emit()
+
+class HistogramPlot(QtWidgets.QLabel):
+    closed = QtCore.Signal()
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Histogram')
+
+    def closeEvent(self, event):
+        event.accept()
+        self.closed.emit()
+    
+class FftPlot(QtWidgets.QLabel):
+    closed = QtCore.Signal()
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('FFT')
+
+    def closeEvent(self, event):
+        event.accept()
+        self.closed.emit()
+
+class FftDistributionPlot(QtWidgets.QLabel):
+    closed = QtCore.Signal()
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('FFT Distribution')
+
+    def closeEvent(self, event):
+        event.accept()
+        self.closed.emit()
+
 class SemTool(QtWidgets.QWidget):
     localImageFolder = None
     
@@ -36,11 +90,20 @@ class SemTool(QtWidgets.QWidget):
         layout.insertWidget(2, self.plotsBox)
         layout.insertWidget(3, self.commandBox)
 
+        self.imagePlot = ImagePlot()
+        self.imagePlot.closed.connect(self.onImagePlotClose)
+        self.histogramPlot = HistogramPlot()
+        self.histogramPlot.closed.connect(self.onHistogramPlotClose)
+        self.fftPlot = FftPlot()
+        self.fftPlot.closed.connect(self.onFftPlotClose)
+        self.fftDistributionPlot = FftDistributionPlot()
+        self.fftDistributionPlot.closed.connect(self.onFftDistributionPlotClose)
+
         try:
             self.sem = SemController()
         except:
             self.semButton.setEnabled(False)
-            print('Could not initialise communication with the SEM, it cannot be used as the image source.')
+            print('Could not initialise communication with the SEM, using it as the image source will not be available.')
 
     def initImageSourceBox(self):
         self.imageSourceBox = QtWidgets.QGroupBox('Image Source')
@@ -77,16 +140,20 @@ class SemTool(QtWidgets.QWidget):
     def initPlotsBox(self):
         self.plotsBox = QtWidgets.QGroupBox('Plots')
 
-        imageBox = QtWidgets.QCheckBox('Image')
-        fftBox = QtWidgets.QCheckBox('FFT')
-        fftDistributionBox = QtWidgets.QCheckBox('FFT Distribution')
-        histogramBox = QtWidgets.QCheckBox('Histogram')
+        self.imageBox = QtWidgets.QCheckBox('Image')
+        self.imageBox.stateChanged.connect(self.onImageBoxToggle)
+        self.fftBox = QtWidgets.QCheckBox('FFT')
+        self.fftBox.stateChanged.connect(self.onFftBoxToggle)
+        self.fftDistributionBox = QtWidgets.QCheckBox('FFT Distribution')
+        self.fftDistributionBox.stateChanged.connect(self.onFftDistributionBoxToggle)
+        self.histogramBox = QtWidgets.QCheckBox('Histogram')
+        self.histogramBox.stateChanged.connect(self.onHistogramBoxToggle)
         
         layout = QtWidgets.QGridLayout()
-        layout.addWidget(imageBox, 0, 0)
-        layout.addWidget(histogramBox, 0, 1)
-        layout.addWidget(fftBox, 1, 0)
-        layout.addWidget(fftDistributionBox, 1, 1)
+        layout.addWidget(self.imageBox, 0, 0)
+        layout.addWidget(self.histogramBox, 0, 1)
+        layout.addWidget(self.fftBox, 1, 0)
+        layout.addWidget(self.fftDistributionBox, 1, 1)
         self.plotsBox.setLayout(layout)
 
     def initCommandBox(self):
@@ -117,6 +184,42 @@ class SemTool(QtWidgets.QWidget):
             self.browseButton.setEnabled(False)
             self.folderText.setEnabled(False)
 
+    def onImageBoxToggle(self, checked):
+        if checked:
+            self.imagePlot.show()
+        else:
+            self.imagePlot.hide()
+
+    def onHistogramBoxToggle(self, checked):
+        if checked:
+            self.histogramPlot.show()
+        else:
+            self.histogramPlot.hide()
+    
+    def onFftBoxToggle(self, checked):
+        if checked:
+            self.fftPlot.show()
+        else:
+            self.fftPlot.hide()
+
+    def onFftDistributionBoxToggle(self, checked):
+        if checked:
+            self.fftDistributionPlot.show()
+        else:
+            self.fftDistributionPlot.hide()
+
+    def onImagePlotClose(self):
+        self.imageBox.setChecked(False)
+
+    def onHistogramPlotClose(self):
+        self.histogramBox.setChecked(False)
+
+    def onFftPlotClose(self):
+        self.fftBox.setChecked(False)
+
+    def onFftDistributionPlotClose(self):
+        self.fftDistributionBox.setChecked(False)
+
     def startFrameUpdate(self):
         if not self.localImageFolder:
             print('Please specify the local folder.')
@@ -145,6 +248,9 @@ class SemTool(QtWidgets.QWidget):
                 self.localImagesIndex = 0
         else:
             image = SemImage.create(self.sem.grabImage())
+
+        if self.imagePlot.isVisible():
+            self.imagePlot.update(image)
 
         self.frameUpdated.emit()
 

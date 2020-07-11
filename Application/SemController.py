18a9bbbf486b718c57b4f5e52eaa9e27e7be6379
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2020-07-12 00:08:18 +0100

    Switch to using memory mapped files for SEM images.

--- Application/SemController.py
+++ Application/SemController.py
@@ -9,26 +9,32 @@
 #   Abbreviations:
 #           ole     Microsoft Object Linking and Embedding document
 
+import os
 import sys
-import tempfile
+import mmap
 from win32com import client
-from PIL import Image
 
 class SemController:
 
     def __init__(self):
         ole = 'CZ.EmApiCtrl.1'
         self.sem = client.Dispatch(ole)
-        self.sem.InitialiseRemoting()
+        self.sem.Initialise()
+
+        self.sem.Grab(0, 0, 1024, 768, 0, 'CZ.MMF')
 
     def __call__(self):
         return self.sem
 
+    def __del__(self):
+        self.mmf.close()
+        os.close(self.file)
+        self.sem.Grab(0, 0, 0, 0, 0, 'CZ.MMF')
+
     def grabImage(self, xOffset=0, yOffset=0, width=1024, height=768, reduction=0):
-        filename = tempfile.TemporaryFile(suffix='.bmp').name
-        self.sem.Grab(xOffset, yOffset, width, height, reduction, filename)
-        image = Image.open(filename)
-        return image
+        self.file = os.open('CZ.MMF')
+        self.mmf = mmap.mmap(self.file.fileno(), 0)
+        return self.mmf
 
 if __name__ == '__main__':
     sem = SemController()

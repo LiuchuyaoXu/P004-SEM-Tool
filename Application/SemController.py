40c1b51f7033293f4c327d9b7bdf28410a9a24c2
Liuchuyao Xu <45833683+LiuchuyaoXu@users.noreply.github.com>
2020-08-01 14:17:12 +0100

    Switch from 'initialise' to 'initialiseRemoting'.

--- Application/SemController.py
+++ Application/SemController.py
@@ -4,37 +4,35 @@
 #
 #   Brief:  Implement the SemController class.
 #           The class gives access to the API of the SEM
-#           The class provides functions that the API does not support directly.
+#           The class provides functions that the API does not directly support.
 #
 #   Abbreviations:
 #           ole     Microsoft Object Linking and Embedding document
 
-import os
 import sys
-import mmap
+import tempfile
 from win32com import client
+from PIL import Image
 
 class SemController:
-
+    
     def __init__(self):
         ole = 'CZ.EmApiCtrl.1'
         self.sem = client.Dispatch(ole)
-        self.sem.Initialise()
-
-        self.sem.Grab(0, 0, 1024, 768, 0, 'CZ.MMF')
+        self.sem.InitialiseRemoting()
 
     def __call__(self):
         return self.sem
 
-    def __del__(self):
-        self.mmf.close()
-        os.close(self.file)
-        self.sem.Grab(0, 0, 0, 0, 0, 'CZ.MMF')
-
     def grabImage(self, xOffset=0, yOffset=0, width=1024, height=768, reduction=0):
-        self.file = os.open('CZ.MMF')
-        self.mmf = mmap.mmap(self.file.fileno(), 0)
-        return self.mmf
+        filename = tempfile.TemporaryFile(suffix='.bmp').name
+        self.sem.Grab(xOffset, yOffset, width, height, reduction, filename)
+        image = Image.open(filename)
+        return image
 
 if __name__ == '__main__':
     sem = SemController()
+    for i in range(5):
+        image = sem.grabImage()
+        image.save('img.png', 'PNG')
+        # image.save('img{}.png'.format(i), 'PNG')
